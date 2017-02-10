5b1ccc223e3958c9c2a85960929d56bed70f7e21
spatankar <shreyaspatankar>
2017-02-09 21:56:53 -0800

    Adding folder

---
+++ Website Drafts/Draft w: in-progress check button/orderDetailUpdater.js
@@ -0,0 +1,15 @@
+function updatePage(cost, coupCode, date, email, firstName, lastName, numGuests, partyName, phone, package, time){
+	// document.getElementById("u3278-4").innerHTML = cost;
+	// document.getElementById("u3276-4").innerHTML = coupCode;
+	// document.getElementById("u3271-4").innerHTML = date;
+	// document.getElementById("u3259-4").innerHTML = email;
+	// document.getElementById("u3257-4").innerHTML = firstName + lastName;
+	// document.getElementById("u3268-4").innerHTML = numGuests;
+	// document.getElementById("u3264-4").innerHTML = partyName;
+	// document.getElementById("u3261-4").innerHTML = phone;
+	// document.getElementById("u3266-4").innerHTML = package;
+	// document.getElementById("u3273-4").innerHTML = time;
+	console.log("start");
+	$('#u3257-4').html(firstName + " " + lastName);
+	console.log("end");
+}
